58a500258a600a4d475f335d954bfd456196eff9
Faridun <akkaunt02uzb@gmail.com>
2023-08-30 17:14:10 +0500

    gap qoshdim 3

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function funksiya() {
         if (x == "") {
             throw "Ismingizni kiriting";
         } else if (parseInt(x)) {
-            throw "Raqam bo'lmasinda eee dscds sdsdsd!";
+            throw "Raqam bo'lmasinda eee dscds sdwerqtgvrymwiu5yjbtvwmkuuuumyusdsd!";
         }
     } catch (err) {
         message.innerHTML = err;
