f299f6975ac935532cd43df582a9ce54a08ce1ae
Faridun <akkaunt02uzb@gmail.com>
2023-08-30 16:56:48 +0500

    dsadsad

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function funksiya() {
         if (x == "") {
             throw "Ismingizni kiriting";
         } else if (parseInt(x)) {
-            throw "Raqam bo'lmasinda eee sdsdsd!";
+            throw "Raqam bo'lmasinda eee dscds sdsdsd!";
         }
     } catch (err) {
         message.innerHTML = err;
